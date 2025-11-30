f766ae63051e66943858087db79741fefaac5797
Clément Rebut <clement.rebut@gmail.com>
2025-11-30 22:22:10 +0100

    Read config file on start

--- main.go
+++ main.go
@@ -1,82 +1,95 @@
 package main
 
 import (
-    "html/template"
-    "fmt"
-    "net/http"
-    "io"
-    "os"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 type ReviewInfo struct {
-    Id uint
-    Author string
-    Title string
-    FilePaths []string
+	Id        uint
+	Author    string
+	Title     string
+	FilePaths []string
 }
 
-func main(){
-    var next_id uint = 0
-    var details_list []ReviewInfo
+type Config struct {
+	Root string `json:"root"`
+}
 
-    new_review_tmpl := template.Must(template.ParseFiles("pages/new_review.html"))
-    new_file_tmpl := template.Must(template.ParseFiles("pages/new_file.html"))
-    details_tmpl := template.Must(template.ParseFiles("pages/review_details.html"))
+func main() {
+	var next_id uint = 0
+	var details_list []ReviewInfo
 
-    fs := http.FileServer(http.Dir("assets/"))
-    http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	config_data, err := os.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal("Could not open config file: ", err)
+	}
+	var config Config
+	err = json.Unmarshal(config_data, &config)
+	if err != nil {
+		log.Fatal("Error while reading config file: ", err)
+	}
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPost {
-            new_review_tmpl.Execute(w, nil)
-            return
-        }
+	new_review_tmpl := template.Must(template.ParseFiles("pages/new_review.html"))
+	new_file_tmpl := template.Must(template.ParseFiles("pages/new_file.html"))
+	details_tmpl := template.Must(template.ParseFiles("pages/review_details.html"))
 
-        details := ReviewInfo{
-            Id: next_id,
-            Author: r.FormValue("author"),
-            Title: r.FormValue("title"),
-        }
-        next_id += 1
+	fs := http.FileServer(http.Dir("assets/"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-        fmt.Println(details)
-        details_list = append(details_list, details)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			new_review_tmpl.Execute(w, nil)
+			return
+		}
 
-        newUrl := fmt.Sprintf("/reviews/%v/addfile", details.Id)
-        http.Redirect(w, r, newUrl, http.StatusSeeOther)
-    })
+		details := ReviewInfo{
+			Id:     next_id,
+			Author: r.FormValue("author"),
+			Title:  r.FormValue("title"),
+		}
+		next_id += 1
 
-    var id uint
-    http.HandleFunc("/reviews/{id}/addfile", func(w http.ResponseWriter, r *http.Request) {
-        details := details_list[id]
+		fmt.Println(details)
+		details_list = append(details_list, details)
 
-        if r.Method != http.MethodPost {
-            new_file_tmpl.Execute(w, details)
-            return
-        }
+		newUrl := fmt.Sprintf("/reviews/%v/addfile", details.Id)
+		http.Redirect(w, r, newUrl, http.StatusSeeOther)
+	})
 
+	var id uint
+	http.HandleFunc("/reviews/{id}/addfile", func(w http.ResponseWriter, r *http.Request) {
+		details := details_list[id]
 
-        file, handler, err := r.FormFile("file")
-        if err != nil {
-            panic(err) //dont do this
-        }
-        defer file.Close()
+		if r.Method != http.MethodPost {
+			new_file_tmpl.Execute(w, details)
+			return
+		}
 
+		file, handler, err := r.FormFile("file")
+		if err != nil {
+			panic(err) //dont do this
+		}
+		defer file.Close()
 
+		details.FilePaths = append(details.FilePaths, handler.Filename)
+		details_list[id] = details
+		fmt.Println(details, r.FormValue("file"))
 
-        details.FilePaths = append(details.FilePaths, handler.Filename)
-        details_list[id] = details
-        fmt.Println(details, r.FormValue("file"))
+		io.Copy(os.Stdout, file)
 
-        io.Copy(os.Stdout, file)
+		new_file_tmpl.Execute(w, details)
+	})
 
-        new_file_tmpl.Execute(w, details)
-    })
+	http.HandleFunc("/reviews/{id}", func(w http.ResponseWriter, r *http.Request) {
+		details := details_list[id]
+		details_tmpl.Execute(w, details)
+	})
 
-    http.HandleFunc("/reviews/{id}", func(w http.ResponseWriter, r *http.Request) {
-        details := details_list[id]
-        details_tmpl.Execute(w, details)
-    })
-
-    http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", nil)
 }
